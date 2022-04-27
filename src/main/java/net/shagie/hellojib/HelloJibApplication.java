2a0b31450acd6635e820ca5d8a9e6a9b45822022
shagie <turnmichael@gmail.com>
2022-04-26 19:05:04 -0500

    chore: Initial files from Spring Initializer

---
+++ src/main/java/net/shagie/hellojib/HelloJibApplication.java
@@ -0,0 +1,13 @@
+package net.shagie.hellojib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/shagie/hellojib/HelloJibApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shagie.hellojib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
